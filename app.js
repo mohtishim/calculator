c14c5183432def6c26e569bd430de56904e6ed6e
mohtishim <mohtishim5@gmail.com>
2025-04-14 16:20:27 +0500

    done2

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 var inputElement = document.getElementById('input1');
+var calculation = false;
 
 function powerFunc(){
     var powerButton = document.getElementById('powerBtn');
     
     if( powerButton.innerText === "OFF"){
         powerButton.innerText = "ON";
+        calculation = true;
         inputElement.value = 0;
-
-        
-        
-        
+       
     }
     else if( powerButton.innerText === "ON"){
         powerButton.innerText = "OFF";
+        calculation = false;
         inputElement.value = "";
+    
     }
 }
 
-if(powerButton.innerText === "OFF"){
-    inputElement.value = ""
-}
+
 
 function calBtn(value) {
+    if (!calculation) return;
     if (inputElement.value === "0") {
         inputElement.value = "";
     }
@@ -29,13 +29,16 @@ function calBtn(value) {
 }
 
 function square(){
+    if (!calculation) return;
     inputElement.value = inputElement.value * inputElement.value;
 }
 
 function root(){
+    if (!calculation) return;
     inputElement.value = Math.sqrt(inputElement.value);
 }
 
 function perform(){
+    if (!calculation) return;
     inputElement.value = eval(inputElement.value)
 }
